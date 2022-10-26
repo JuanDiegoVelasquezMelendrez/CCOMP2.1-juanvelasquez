c897da125fc2d1f950359f40358660946aaed862
JuanDiegoVelasquezMelendrez <juan.velasquez.melendrez@ucsp.edu.pe>
2022-10-26 07:13:32 -0500

    new_commit

---
+++ Práctica Parciales/Figuras del Libro/fig07_05.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+#include <array>
+
+using namespace std;
+
+int main() {
+
+    const size_t arraySize{5};
+
+    array <int, arraySize> values;
+
+    for (size_t i{0}; i < values.size(); ++i)
+    {
+        values[i] = 2 + 2 * i;
+    }
+
+    cout << "ELement" << setw(10) << "Value" << endl;
+    
+    for (size_t j{0}; j < values.size(); ++j)
+    {
+        cout << setw(7) << j << setw(10) << values[j] << endl;
+    }
+
+    return 0;
+}
